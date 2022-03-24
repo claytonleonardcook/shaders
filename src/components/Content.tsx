0c961441f8ecfb9a74c0c05d8d2a01e659746bec
claytonleonardcook <claytonleonardcook@gmail.com>
2022-03-24 14:03:58 -0400

    edit layout

--- src/components/Content.tsx
+++
@@ -1,9 +0,0 @@
-type Props = {
-  children: JSX.Element;
-};
-
-const Content = ({ children }: Props) => {
-  return <main className="Content flex-grow">{children}</main>;
-};
-
-export default Content;
--- src/components/Header.tsx
+++
@@ -1,5 +0,0 @@
-const Header = () => {
-    return <header>Header here. Could be a navbar or something.</header>;
-};
-
-export default Header;
